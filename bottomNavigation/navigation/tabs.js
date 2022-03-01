576b823d84f55a1aff8c84e3ff48e4be9da6c0e2
Student-AY <Student-Ahmet-Yilmaz@hotmail.com>
2022-03-01 15:59:29 +0100

    ...

--- bottomNavigation/navigation/tabs.js
+++ bottomNavigation/navigation/tabs.js
@@ -1,5 +1,5 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-
+import Ionicons from 'react-native-vector-icons/Ionicons';
 import HomeScreen from '../screens/HomeScreen';
 import FindScreen from '../screens/FindScreen';
 import ChatScreen from '../screens/ChatScreen';
@@ -10,14 +10,40 @@ const Tab = createBottomTabNavigator();
 
 const Tabs = () => {
     return(
-        <Tab.Navigator>
-            <Tab.Screen name='Home' component={HomeScreen}/>
-            <Tab.Screen name='Find' component={FindScreen}/>
-            <Tab.Screen name='Post' component={PostScreen}/>
-            <Tab.Screen name='Settings' component={SettingsScreen}/>
-            <Tab.Screen name='Chat' component={ChatScreen}/>
+        <Tab.Navigator
+        screenOptions={{
+            tabBarShowLabel:false,
+            tabBarStyle:{backgroundColor:'cyan' , margin:15 , shadowColor:'black',borderRadius:15},
+        }}
+        >
 
-
+            <Tab.Screen options={{
+                tabBarIcon : ({color,size}) => {
+                 return   <Ionicons name='md-home'/>
+                },
+            }} name='Home' component={HomeScreen}/>
+            <Tab.Screen options={{
+                tabBarIcon : ({color , size}) => {
+                    return <Ionicons name='md-search'/>
+                },
+            }} name='Find' component={FindScreen}/>
+            <Tab.Screen options={{
+                tabBarIcon : ({color , size}) => {
+                    return <Ionicons name='md-add-circle'/>
+                },
+            }} name='Post' component={PostScreen}/>
+            <Tab.Screen options={{
+                tabBarIcon : ({color , size}) => {
+                    return <Ionicons name='md-settings'/>
+                },
+            }} name='Settings' component={SettingsScreen}/>
+            <Tab.Screen options={{
+                tabBarIcon : ({color , size}) => {
+                    return <Ionicons name='md-chatbubbles'/>
+                },
+            }} name='Chat' component={ChatScreen}/>
         </Tab.Navigator>
     )
 }
+
+export default Tabs;
